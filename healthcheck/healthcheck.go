6c818daf0c0b79fd11409f7f8250f10f79acf22d
Nguyen Ta <nguyen.ta@gogox.com>
2023-04-18 16:56:00 +0700

    Add sampling for tracing

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,16 +1,14 @@
 package healthcheck
 
 import (
-	"bufio"
 	"context"
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v9"
 	"github.com/gogovan/ggx-kr-service-utils/constants"
+	"github.com/gogovan/ggx-kr-service-utils/http/middlewares"
+	"github.com/gogovan/ggx-kr-service-utils/saga/retry"
 	"github.com/jmoiron/sqlx"
 	"github.com/segmentio/kafka-go"
-	"net"
-	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gogovan/ggx-kr-service-utils/logger"
@@ -25,52 +23,31 @@ func Run(
 	writeDb *sqlx.DB,
 	redis redis.UniversalClient,
 	client *kafka.Conn,
+	kafkaConnectFunc func() (*kafka.Conn, error),
 ) func() {
 	return func() {
-		health := healthcheck.NewHandler()
-
-		livenessCheck(ctx, cfg.GoroutineThreshold, health)
 		itv := time.Duration(cfg.Interval) * time.Second
-		readinessCheck(ctx, health, itv, readDb, writeDb, redis, client)
-
-		logMd := func(next http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				// Our middleware logic goes here...
-				rw := NewResponseWriter(w)
-				next.ServeHTTP(rw, r)
-				statusCode := rw.Code()
-				logger.Info(
-					"Response information",
-					zap.String("status_code", strconv.Itoa(statusCode)),
-					zap.String("Method", r.Method),
-					zap.String("URL", r.RequestURI),
-				)
-			})
-		}
-
+		health := healthcheck.NewHandler()
+		readyCheck(ctx, cfg.GoroutineThreshold, health)
+		liveCheck(ctx, health, itv, readDb, writeDb, redis, client, kafkaConnectFunc)
+		gin.SetMode(gin.ReleaseMode)
+		router := gin.New()
+		router.Use(middlewares.Logging(logger.GetDefaultLogger()))
+		router.Use(gin.WrapH(health))
 		logger.Info("Heathcheck server listening on port", zap.String("Port", cfg.Port))
-		if err := http.ListenAndServe(cfg.Port, logMd(health)); err != nil {
+		if err := router.Run(cfg.Port); err != nil {
 			logger.Warn("Heathcheck server", zap.Error(err))
 		}
 	}
 }
 
-func livenessCheck(ctx context.Context, goRoutinesThreshold int, health healthcheck.Handler) {
-	health.AddLivenessCheck(constants.GoroutineThreshold, healthcheck.GoroutineCountCheck(goRoutinesThreshold))
+func readyCheck(ctx context.Context, goRoutinesThreshold int, health healthcheck.Handler) {
+	health.AddReadinessCheck(constants.GoroutineThreshold, healthcheck.GoroutineCountCheck(goRoutinesThreshold))
 }
 
-func readinessCheck(
-	ctx context.Context,
-	health healthcheck.Handler,
-	interval time.Duration,
-	readDb *sqlx.DB,
-	writeDb *sqlx.DB,
-	redis redis.UniversalClient,
-	client *kafka.Conn,
-) {
-
+func liveCheck(ctx context.Context, health healthcheck.Handler, interval time.Duration, readDb *sqlx.DB, writeDb *sqlx.DB, redis redis.UniversalClient, client *kafka.Conn, connectFunc func() (*kafka.Conn, error)) {
 	if readDb != nil {
-		health.AddReadinessCheck(constants.ReadDatabase, healthcheck.AsyncWithContext(ctx, func() (err error) {
+		health.AddLivenessCheck(constants.ReadDatabase, healthcheck.AsyncWithContext(ctx, func() (err error) {
 			err = readDb.DB.PingContext(ctx)
 			if err != nil {
 				logger.Error("Read database", zap.Error(err))
@@ -79,7 +56,7 @@ func readinessCheck(
 		}, interval))
 	}
 	if writeDb != nil {
-		health.AddReadinessCheck(constants.WriteDatabase, healthcheck.AsyncWithContext(ctx, func() (err error) {
+		health.AddLivenessCheck(constants.WriteDatabase, healthcheck.AsyncWithContext(ctx, func() (err error) {
 			err = writeDb.DB.PingContext(ctx)
 			if err != nil {
 				logger.Error("Readiness check write database", zap.Error(err))
@@ -89,7 +66,7 @@ func readinessCheck(
 	}
 
 	if redis != nil {
-		health.AddReadinessCheck(constants.Redis, healthcheck.AsyncWithContext(ctx, func() error {
+		health.AddLivenessCheck(constants.Redis, healthcheck.AsyncWithContext(ctx, func() error {
 			err := redis.Ping(ctx).Err()
 			if err != nil {
 				logger.Error("Redis Readiness Check Fail", zap.Error(err))
@@ -99,77 +76,21 @@ func readinessCheck(
 	}
 
 	if client != nil {
-		health.AddReadinessCheck(constants.Kafka, healthcheck.AsyncWithContext(ctx, func() error {
+		health.AddLivenessCheck(constants.Kafka, healthcheck.AsyncWithContext(ctx, func() error {
 			_, err := client.Brokers()
 			if err != nil {
+				err := retry.NewBackoff(
+					retry.WithBackoffInitialInterval(3*time.Second),
+					retry.WithBackoffMaxRetries(3)).Retry(ctx, func() error {
+					client, err = connectFunc()
+					if err != nil {
+						logger.Error("Retry connecting to kafka ...")
+					}
+					return err
+				})
 				logger.Error("Kafka Readiness Check Fail", zap.Error(err))
 			}
 			return err
 		}, interval))
 	}
-}
-
-type ResponseWriter struct {
-	http.ResponseWriter
-
-	code int
-	size int
-}
-
-// Returns a new `ResponseWriter` type by decorating `http.ResponseWriter` type.
-func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
-	return &ResponseWriter{
-		ResponseWriter: w,
-	}
-}
-
-// Overrides `http.ResponseWriter` type.
-func (r *ResponseWriter) WriteHeader(code int) {
-	if r.Code() == 0 {
-		r.code = code
-		r.ResponseWriter.WriteHeader(code)
-	}
-}
-
-// Overrides `http.ResponseWriter` type.
-func (r *ResponseWriter) Write(body []byte) (int, error) {
-	if r.Code() == 0 {
-		r.WriteHeader(http.StatusOK)
-	}
-
-	var err error
-	r.size, err = r.ResponseWriter.Write(body)
-
-	return r.size, err
-}
-
-// Overrides `http.Flusher` type.
-func (r *ResponseWriter) Flush() {
-	if fl, ok := r.ResponseWriter.(http.Flusher); ok {
-		if r.Code() == 0 {
-			r.WriteHeader(http.StatusOK)
-		}
-
-		fl.Flush()
-	}
-}
-
-// Overrides `http.Hijacker` type.
-func (r *ResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	hj, ok := r.ResponseWriter.(http.Hijacker)
-	if !ok {
-		return nil, nil, fmt.Errorf("the hijacker interface is not supported")
-	}
-
-	return hj.Hijack()
-}
-
-// Returns response status code.
-func (r *ResponseWriter) Code() int {
-	return r.code
-}
-
-// Returns response size.
-func (r *ResponseWriter) Size() int {
-	return r.size
 }
