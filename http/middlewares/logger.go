5ac27fca5890888a60ed7a7cef10af515fa11936
Nguyen Ta <nguyen.ta@gogox.com>
2023-04-19 20:25:06 +0700

    Another logging interface

--- http/middlewares/logger.go
+++ http/middlewares/logger.go
@@ -28,6 +28,50 @@ func (w bodyLogWriter) Write(b []byte) (int, error) {
 	return w.ResponseWriter.Write(b)
 }
 
+func RequestLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		var body []byte
+		var buf bytes.Buffer
+		tee := io.TeeReader(c.Request.Body, &buf)
+		body, _ = io.ReadAll(tee)
+		c.Request.Body = io.NopCloser(&buf)
+		c.Next()
+		fields := []zapcore.Field{
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", c.Request.URL.Path),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+		}
+
+		if len(c.Request.URL.RawQuery) > 0 {
+			fields = append(fields, zap.String("query", c.Request.URL.RawQuery))
+		}
+
+		if requestID := c.Writer.Header().Get("X-Request-Id"); requestID != "" {
+			fields = append(fields, zap.String("request_id", requestID))
+		}
+
+		if span := trace.SpanFromContext(c.Request.Context()).SpanContext(); span.IsValid() {
+			fields = append(fields, zap.String("trace_id", span.TraceID().String()))
+		}
+
+		if len(body) > 0 {
+			fields = append(fields, zap.String("body", string(body)))
+		}
+		fields = append(fields, zap.Duration("latency", time.Since(start)))
+		if len(c.Errors) > 0 {
+			// Append error field if this is an erroneous request.
+			for _, e := range c.Errors.Errors() {
+				logger.Error(e, fields...)
+			}
+		} else {
+			logger.Info(c.Request.URL.Path, fields...)
+		}
+	}
+}
+
 func Logging(logger logger.Logger, skips ...string) gin.HandlerFunc {
 	return ginzap.GinzapWithConfig(logger.GetZapLogger(), &ginzap.Config{
 		UTC:        true,
@@ -43,12 +87,6 @@ func Logging(logger logger.Logger, skips ...string) gin.HandlerFunc {
 				fields = append(fields, zap.String("trace_id", span.TraceID().String()))
 				fields = append(fields, zap.String("span_id", span.SpanID().String()))
 			}
-			var body []byte
-			var buf bytes.Buffer
-			tee := io.TeeReader(c.Request.Body, &buf)
-			body, _ = io.ReadAll(tee)
-			c.Request.Body = io.NopCloser(&buf)
-			fields = append(fields, zap.String("body", string(body)))
 			return fields
 		},
 	})
